46d98833413cf9fd3aae1516bfae5fe1524877a5
alexeykozlov <alexeymspm@gmail.com>
2021-03-03 18:32:07 -0500

    TicketMonster testing

---
+++ src/test/java/com/k12/TicketMonsterTest.java
@@ -0,0 +1,57 @@
+package com.k12;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TicketMonsterTest {
+
+    private static final AutomatedBrowserFactory AUTOMATED_BROWSER_FACTORY =
+            new AutomatedBrowserFactory();
+
+    private static final int WAIT_TIME = 30;
+
+    @Test
+    public void purchaseTickets() {
+
+        final AutomatedBrowser automatedBrowser =
+                AUTOMATED_BROWSER_FACTORY.getAutomatedBrowser("ChromeNoImplicitWait");
+
+        try {
+
+            automatedBrowser.init();
+
+            automatedBrowser.goTo("https://ticket-monster.herokuapp.com");
+
+            automatedBrowser.clickElement("Buy tickets now", WAIT_TIME);
+
+            automatedBrowser.clickElement("Concert", WAIT_TIME);
+
+            automatedBrowser.clickElement("Rock concert of the decade", WAIT_TIME);
+
+            automatedBrowser.selectOptionByTextFromSelect("Toronto : Roy Thomson Hall", "venueSelector", WAIT_TIME);
+
+            automatedBrowser.clickElement("bookButton", WAIT_TIME);
+
+            automatedBrowser.selectOptionByTextFromSelect("A - Premier platinum reserve", "sectionSelect", WAIT_TIME);
+
+            automatedBrowser.populateElement("tickets-1", "2", WAIT_TIME);
+
+            automatedBrowser.clickElement("add", WAIT_TIME);
+
+            automatedBrowser.populateElement("email", "email@example.org", WAIT_TIME);
+
+            automatedBrowser.clickElement("submit", WAIT_TIME);
+
+            final String email = automatedBrowser.getTextFromElement("#content > div:nth-child(2) > div:nth-child(1) > div > p:nth-child(2)", WAIT_TIME);
+            Assert.assertTrue(email.contains("email@example.org"));
+
+            final String event = automatedBrowser.getTextFromElement("#content > div:nth-child(2) > div:nth-child(1) > div > p:nth-child(3)", WAIT_TIME);
+            Assert.assertTrue(event.contains("Rock concert of the decade"));
+
+            final String venue = automatedBrowser.getTextFromElement("div.col-md-6:nth-child(1) > div:nth-child(1) > p:nth-child(4)", WAIT_TIME);
+            Assert.assertTrue(venue.contains("Roy Thomson Hall"));
+        } finally {
+            automatedBrowser.destroy();
+        }
+    }
+}
